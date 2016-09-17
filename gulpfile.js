fe325d9f35599eb684f6799d3ea2a26f619c5fae
lsy <ujnlsy@126.com>
2016-09-17 23:36:58 +0800

    gulp启动

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
 var gulp = require('gulp'),
+
+    connect = require('gulp-connect'),
+
     less = require('gulp-less'),
     cssmin = require('gulp-minify-css'),
-    sourcemap = require('gulp-sourcemaps');
+    sourcemap = require('gulp-sourcemaps'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber');
 
+gulp.task('server', () => {
+    connect.server({
+        port: 8282,
+        livereload: true
+    })
+});
 
-gulp.task('less', function() {
+gulp.task('less', () => {
     // 将你的默认的任务代码放在这
     gulp.src('src/assets/*.less')
+        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
         .pipe(less())
         .pipe(cssmin())
         .pipe(sourcemap.write())
         .pipe(gulp.dest('dist/css'));
 });
+
+gulp.task('watchLess', () => {
+    gulp.watch('src/assets/*.less', ['less'])
+});
+
+gulp.task('dev', ['server', 'watchLess']);
